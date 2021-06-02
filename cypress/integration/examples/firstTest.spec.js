5d396ad5c7997738962d208ade5209295b6b8922
Ekaterina Savicheva <ekaterina_savicheva@lineate.com>
2021-06-02 15:58:11 +0400

    test

---
+++ cypress/integration/examples/firstTest.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+it('Using Get and Find with Eq', () => {
+    cy.visit('https://next.privat24.ua/deposit/open')
+    cy.get('tbody').find('td').find('div').find('button').eq(0)
+});
+
+it.only('Using Get and Find with Eq', () => {
+    cy.viewport(1800, 700)
+    cy.visit('https://docs.cypress.io/api/commands/eq')
+    cy.get('[class^="hidden"]').find('[href="#Syntax"]').click()
+    //cy.get('div').find('nav').find('ul').find('li').find('a')
+});
